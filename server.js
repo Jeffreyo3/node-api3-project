431609ed58dc688764c8ba47ebf0af7f7e52bb48
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-01-22 03:16:36 -0800

    Add helmet and morgan middlewares
    Create custom middleware for logger

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const server = express();
+
+
+//implement middleware
+server.use(
+  express.json(),
+  helmet(),
+  morgan('dev'),
+  logger
+);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +19,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.url} at ${new Date().toISOString()}`);
+  next();
+}
+
 
 module.exports = server;
