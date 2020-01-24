334b94af1f9660c93394eb24b5ac6a42ad6c7200
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-01-23 20:12:34 -0800

    Update server '/' res

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>Middleware project is alive!!</h2>`);
 });
 
 //custom middleware
